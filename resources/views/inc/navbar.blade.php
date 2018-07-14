acefbb51f600ec4569cf27b0e5203907496d35bf
Arnachap <arnaud.chapeleur@gmail.com>
2018-07-14 11:17:25 +0200

    Main navbar styling

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-md navbar-light fixed-top navbar-laravel">
+<nav class="navbar navbar-expand-md fixed-top mainNav">
     <div class="container">
         <a class="navbar-brand" href="{{ url('/') }}">
             {{ config('app.name', 'Boucherie Centrale') }}
@@ -11,16 +11,16 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <!-- Left Side Of Navbar -->
             <ul class="navbar-nav ml-auto">
-                <li class="nav-item">
+                <li class="nav-item {{ Request::is('nos-produits') ? 'current' : '' }}">
                     <a href="/nos-produits" class="nav-link">Nos produits</a>
                 </li>
-                <li class="nav-item">
+                <li class="nav-item {{ Request::is('service-traiteur') ? 'current' : '' }}">
                     <a href="/service-traiteur" class="nav-link">Service - traiteur</a>
                 </li>
-                <li class="nav-item">
+                <li class="nav-item {{ Request::is('a-propos') ? 'current' : '' }}">
                     <a href="/a-propos" class="nav-link"> A propos</a>
                 </li>
-                <li class="nav-item">
+                <li class="nav-item {{ Request::is('nous-contacter') ? 'current' : '' }}">
                     <a href="/nous-contacter" class="nav-link">Contact</a>
                 </li>
             </ul>
