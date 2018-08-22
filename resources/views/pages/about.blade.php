dff495700551b840ce01f4d364b15a80838d5c60
Arnachap <arnaud.chapeleur@gmail.com>
2018-08-22 17:25:04 +0200

    About and contact styling

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,7 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1 class="text-center my-4">A propos de nous</h1>
-
-    <img class="rounded mx-auto d-block" src="./img/team.jpg" alt="L'équipe Boucherie Centrale Malzéville">
+    <section class="about">
+        <h1 class="text-center my-4">A propos de nous</h1>
+    
+        <img class="rounded mx-auto d-block" src="./img/team.jpg" alt="L'équipe Boucherie Centrale Malzéville">
+    </section>
 @endsection
--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -2,43 +2,44 @@
 
 @section('content')
 
-<div class="container">
-    <div class="py-5 text-center">
-        <h1 class="text-center my-4">Nous contacter</h1>
-        <p class="lead">Une demande de devis ? Un renseignement ?
+<section class="contact">
+    <div class="container">
+        <h1>Nous contacter</h1>
+        <p class="intro">Une demande de devis ? Un renseignement ?
             <br> N'hésitez pas à nous contacter, nous répondrons à votre requête au plus vite.</p>
-    </div>
-
-    <div class="row">
-        <div class="col-8 mx-auto">
-            <h4 class="mb-3">Vos coordonées</h4>
-            <form class="needs-validation">
-                <div class="mb-3">
-                    <label for="name">Votre nom</label>
-                    <input type="text" class="form-control" id="name" placeholder="Nom" required>
-                </div>
-
-                <div class="mb-3">
-                    <label for="email">Votre email</label>
-                    <input type="email" class="form-control" id="email" placeholder="Email" required>
-                </div>
-
-                <div class="mb-3">
-                    <label for="subject">Sujet</label>
-                    <input type="text" class="form-control" id="subject" placeholder="Sujet" required>
-                </div>
-
-                <div class="mb-3">
-                    <label for="message">Votre message</label>
-                    <textarea type="text" rows="5" class="form-control" id="message" placeholder="Votre message"></textarea>
-                </div>
-
-                <hr class="mb-4">
-
-                <button class="btn btn-primary btn-lg d-block mx-auto" type="submit">Envoyer</button>
-            </form>
+    
+        <div class="row">
+            <div class="col-8 mx-auto">
+                <form class="needs-validation">
+                    <div class="form-field">
+                        <label for="name">Votre nom</label>
+                        <input type="text" class="form-control" id="name" placeholder="Nom" required>
+                    </div>
+    
+                    <div class="form-field">
+                        <label for="email">Votre email</label>
+                        <input type="email" class="form-control" id="email" placeholder="Email" required>
+                    </div>
+    
+                    <div class="form-field">
+                        <label for="subject">Sujet</label>
+                        <input type="text" class="form-control" id="subject" placeholder="Sujet" required>
+                    </div>
+    
+                    <div class="form-field">
+                        <label for="message">Votre message</label>
+                        <textarea type="text" rows="5" class="form-control" id="message" placeholder="Votre message"></textarea>
+                    </div>
+    
+                    <hr>
+    
+                    <div class="button">
+                        <input class="btn btn-secondary btn-lg" type="submit" value="Envoyer">
+                    </div>
+                </form>
+            </div>
         </div>
     </div>
-</div>
+</section>
 
 @endsection
